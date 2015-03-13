3bb129032be905e2e6c63e69f731aaa8dee5faa3
annapetuhova <cosmostar@mail.ru>
2015-03-13 23:41:35 +0500

    L

---
+++ controllers.js
@@ -0,0 +1,47 @@
+﻿angular
+    .module("app", ["filtersModule", "directivesModule"])
+    .controller("Contorller1", ["$scope",
+        function ($scope) {
+
+            $scope.name = "Запись";
+
+            $scope.filterName = "";
+
+            $scope.items = [];
+            $scope.items2 = [];
+            $scope.newItem = {
+                name: "",
+                priority: "",
+                ispoln: ""
+            };
+            $scope.add = function () {
+                if (($scope.newItem.name != "") && ($scope.newItem.ispoln != "") && (($scope.newItem.priority == "Высокий") || ($scope.newItem.priority == "Средний") || ($scope.newItem.priority == "Низкий"))) {
+                    $scope.items.push({
+                        name: $scope.newItem.name,
+                        priority: $scope.newItem.priority,
+                        ispoln: $scope.newItem.ispoln
+                    });
+                    $scope.newItem.name = "";
+                    $scope.newItem.priority = "";
+                    $scope.newItem.ispoln = "";
+                } else {
+                    alert("Пожалуйста,заполните все поля!");
+                }
+            };
+
+
+            $scope.del = function (item) {
+                var index = $scope.items.indexOf(item);
+                $scope.items.splice(index, 1);
+
+            }
+            $scope.replace = function (mname, mpr, misp, i) {
+                $scope.items2.push({
+                    name: mname,
+                    priority: mpr,
+                    ispoln: misp
+                });
+                $scope.del(i);
+            };
+
+        }]);
---
+++ directives.js
@@ -0,0 +1,26 @@
+﻿angular
+    .module("directivesModule", [])
+    .directive("testcolor", function () {
+
+        return {
+            restrict: "A",
+            require: "?ngModel",
+            link: function ($scope, $element, attrs, ngModel) {
+                if ($scope.i.priority == "Высокий") {
+
+                    $element.css({ "color": "red" });
+
+                }
+                if ($scope.i.priority == "Средний") {
+
+                    $element.css({ "color": "yellow" });
+
+                }
+                if ($scope.i.priority == "Низкий") {
+
+                    $element.css({ "color": "green" });
+
+                }
+            }
+        };
+    });
